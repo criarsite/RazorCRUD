0d3e1268d3b7265d8d571fd0e2f0e5f4cd30da25
Washington <106989270+criarsite@users.noreply.github.com>
2022-11-12 04:06:14 +0000

    Atualizando

---
+++ Pages/Produtos/Criar.cshtml.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using RazorCRUD.Data;
+using RazorCRUD.Models;
+
+namespace RazorCRUD.Pages.Produtos
+{
+    public class Criar : PageModel
+    {
+        private readonly Contexto _db;
+
+        public Criar(Contexto contexto)
+        {
+            _db = contexto;
+        }
+        [BindProperty]
+        public Produto Producto { get; set; }
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if(!ModelState.IsValid)
+            {
+            return Page();
+            }
+
+            Producto.DataCadastro = DateTime.Now;
+            _db.Add(Producto);
+            await _db.SaveChangesAsync();
+return RedirectToPage("Index");
+
+        }
+    }
+}
